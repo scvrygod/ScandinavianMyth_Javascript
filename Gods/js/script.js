87e3af07773687063c3942e808ec74a758d43a54
scvrygod <81991161+scvrygod@users.noreply.github.com>
2021-11-01 16:11:48 +0100

    Add files via upload

---
+++ Gods/js/script.js
@@ -0,0 +1,141 @@
+var imageOdin = document.querySelector('.image_odin');
+
+let i=0;
+function playOdin(){
+    if(i==0)
+    {
+        imageOdin.style.backgroundImage='url(G:/webs/Dark/Gods/images/1.jpg)';
+        i++;
+    }
+    else if(i==1)
+    {
+        imageOdin.style.backgroundImage='url(G:/webs/Dark/Gods/images/2.jpg)';
+        i++;
+    }
+    else if(i==2)
+    {
+        
+        imageOdin.style.backgroundImage='url(G:/webs/Dark/Gods/images/3.jpg)';
+        i++;
+    }
+    else if(i==3)
+    {
+        imageOdin.style.backgroundImage='';
+        i=0;
+    }
+   
+}
+
+var imageThor = document.querySelector('.image_thor');
+
+let i1=0;
+function playThor(){
+    if(i1==0)
+    {
+        imageThor.style.backgroundImage='url(G:/webs/Dark/Gods/images/Thor1.jpg)';
+        i1++;
+    }
+    else if(i1==1)
+    {
+        imageThor.style.backgroundImage='url(G:/webs/Dark/Gods/images/Thor2.jpg)';
+        i1++;
+    }
+    else if(i1==2)
+    {
+        
+        imageThor.style.backgroundImage='url(G:/webs/Dark/Gods/images/Thor3.jpg)';
+        i1++;
+    }
+    else if(i1==3)
+    {
+        imageThor.style.backgroundImage='';
+        i1=0;
+    }
+   
+}
+
+var imageLoki = document.querySelector('.image_loki');
+let i2=0;
+function playLoki(){
+    if(i2==0)
+    {
+        imageLoki.style.backgroundImage='url(G:/webs/Dark/Gods/images/Loki1.jpg)';
+        i2++;
+    }
+    else if(i2==1)
+    {
+        imageLoki.style.backgroundImage='url(G:/webs/Dark/Gods/images/Loki2.jpg)';
+        i2++;
+    }
+    else if(i2==2)
+    {
+        
+        imageLoki.style.backgroundImage='url(G:/webs/Dark/Gods/images/Loki3.jpg)';
+        imageLoki.style.width="1000px";
+        imageLoki.style.heigth="1200px";
+        i2++;
+    }
+    else if(i2==3)
+    {
+        imageLoki.style.backgroundImage='';
+        imageLoki.style.width="";
+        imageLoki.style.heigth="";
+        i2=0;
+    }
+   
+}
+
+var imageFrigg = document.querySelector('.image_frigg');
+let i3=0;
+function playFrigg(){
+    if(i3==0)
+    {
+        imageFrigg.style.backgroundImage='url(G:/webs/Dark/Gods/images/Frigg1.jpg)';
+        i3++;
+    }
+    else if(i3==1)
+    {
+        imageFrigg.style.backgroundImage='url(G:/webs/Dark/Gods/images/Frigg2.jpg)';
+        i3++;
+    }
+    else if(i3==2)
+    {
+        
+        imageFrigg.style.backgroundImage='url(G:/webs/Dark/Gods/images/Frigg3.jpg)';
+        i3++;
+    }
+    else if(i3==3)
+    {
+        imageFrigg.style.backgroundImage='';
+        i3=0;
+    }
+   
+}
+
+
+var imageBaldur = document.querySelector('.image_baldur');
+let i4=0;
+function playBaldur(){
+    if(i4==0)
+    {
+        imageBaldur.style.backgroundImage='url(G:/webs/Dark/Gods/images/Baldur1.jpg)';
+        i4++;
+    }
+    else if(i4==1)
+    {
+        imageBaldur.style.backgroundImage='url(G:/webs/Dark/Gods/images/Baldur2.jpg)';
+        i4++;
+    }
+    else if(i4==2)
+    {
+        
+        imageBaldur.style.backgroundImage='url(G:/webs/Dark/Gods/images/Baldur3.jpg)';
+        i4++;
+    }
+    else if(i4==3)
+    {
+        imageBaldur.style.backgroundImage='';
+        i4=0;
+    }
+   
+}
